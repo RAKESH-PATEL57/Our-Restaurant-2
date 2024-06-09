b808dc241ba66b9454c70644d08f85f13ac838c9
Rakesh Patel <rakesh246808@gmail.com>
2024-06-09 10:57:31 +0530

    created dynamic dishes Data for home-page

---
+++ dynamicData.js
@@ -0,0 +1,44 @@
+// front page dishes
+"use strict"
+import introDishes from "./dishes/introFood.json" with {type: 'json'};
+
+console.log(introDishes);
+
+
+for(let i=0;i<3;i++)
+{
+    console.log(name);
+}
+
+let introDishesContainer = document.querySelector('.intro-dishes-container');
+
+introDishes.forEach((index,value) =>{
+    
+    const introDishesContents = `
+    <div class="intro-dishes-card">
+    <div class="content">
+        <div class="dish-details">
+            <h1>${introDishes[value].dishName}</h1>
+            <p>${introDishes[value].price}</p>
+        </div>
+        <div class="rating">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+        </div>
+    </div>
+    <img src="${imageLink(value,introDishes)}" alt="chicken">
+    <button class="btn">Add to Cart</button>
+    </div>
+    `
+    introDishesContainer.insertAdjacentHTML("beforeend", introDishesContents);
+})
+
+function imageLink(value,introDishes)
+{
+    let link = ((introDishes[value].image).toString()).slice(1);
+
+    return link;
+}
