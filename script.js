e8f21a85ab82f05e7ebfbc4eab874a25dcd1de98
Rakesh Patel <rakesh246808@gmail.com>
2024-06-25 08:33:33 +0530

    working on review section

--- script.js
+++ script.js
@@ -8,5 +8,36 @@ cartOpen.addEventListener("click", () => {
 
 closeBtn.addEventListener("click", () => {
     cartContainer.classList.remove("cart-show-hide");
+});
+
+// review stars section    [[[[[ Start  ]]]]]
+let stars = document.querySelectorAll(".stars");
+let starClick = 0;
+if(starClick == 0)
+{
+
+    stars.forEach((star, index) => {
+        stars[index].addEventListener("mouseover",() => {
+            // stars[index].style.background = "aqua";
+        for(let i = 0 ; i <= index ; i++)
+        {
+            stars[i].style.color = "aqua";
+            stars[i].addEventListener("onclick", () => {
+                starClick = 1;
+            });
+        }
+        console.log(index);
+    });
+    if(starClick == 0) {
+        stars[index].addEventListener("mouseout",() => {
+            // stars[index].style.background = "aqua";
+            stars[index].style.color = "white";
+            console.log(index);
+        });
+    }
 })
+}
+
+// review stars section    [[[[[  End   ]]]]]
+
 
