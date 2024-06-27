a230b78d1d76322b3b53dc6f4996abb7fb940961
Rakesh Patel <rakesh246808@gmail.com>
2024-06-27 21:34:17 +0530

    working on review section

--- script.js
+++ script.js
@@ -12,46 +12,38 @@ closeBtn.addEventListener("click", () => {
 
 // review stars section    [[[[[ Start  ]]]]]
 let stars = document.querySelectorAll(".stars");
-let starClick = 0;
-if(starClick == 0)
-{
 
-    stars.forEach((star, index) => {
-        stars[index].addEventListener("mouseover",() => {
-            // stars[index].style.background = "aqua";
-        for(let i = 0 ; i <= index ; i++)
-        {
-            stars[i].style.color = "aqua";
-            stars[i].addEventListener("onclick", () => {
-                starClick = 1;
-            });
-        }
-        console.log(index);
+stars.forEach((star, i) => {
+    star.addEventListener("click" , () => {
+        let currentStar = i + 1;
+        stars.forEach((star, j) => {
+            if(currentStar >= j+1)
+            {
+                star.classList.add("toggleActiveStar");
+            }
+            else
+            {
+                star.classList.remove("toggleActiveStar");
+            }
+        })
     });
-    if(starClick == 0) {
-        stars[index].addEventListener("mouseout",() => {
-            // stars[index].style.background = "aqua";
-            stars[index].style.color = "white";
-            console.log(index);
-        });
-    }
+
+    // ["click", "mouseover"].forEach((event) => {
+    //       star.addEventListener(event, test(i));
+    // });
 })
-}
 
-// review stars section    [[[[[  End   ]]]]]
-
-// highliting the section which is open 
-let section = document.querySelectorAll("nav ul li a");
-
-let initial = 0;
-section.forEach((sec, index) => {
-    section[index].addEventListener("click", () => {
-        section[index].classList.add("active");
-        section[initial].classList.remove("active");
-        console.log(index);
-        initial = index;
-    });
-
-});
-
-
+// function test(i) 
+// {
+//     let currentStar = i + 1;
+//     stars.forEach((star, j) => {
+//         if(currentStar >= j+1)
+//         {
+//             star.classList.add("toggleActiveStar");
+//         }
+//         else
+//         {
+//             star.classList.remove("toggleActiveStar");
+//         }
+//     })
+// }
